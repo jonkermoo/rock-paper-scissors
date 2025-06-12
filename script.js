1aec6bede99ec0cfd8ac8fac9645694e952295d7
Johnny Zhu <zhu.johnny0@gmail.com>
2025-06-11 21:31:52 -0400

    added script to make buttons change color
    when buttons are clicked, it will turn gray then white upon release

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
 let humanChoice;
 let cpuChoice;
 
+/* humanChoice depends on what button clicked */
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissor = document.querySelector(".scissor");
+
+
+const click = document.querySelectorAll(".click");
+
+click.forEach(button => {
+    button.addEventListener("mousedown", () => {
+        button.style.backgroundColor = "gray";
+    });
+});
+
+click.forEach(button => {
+    button.addEventListener("mouseup", () => {
+        button.style.backgroundColor = "rgb(241, 241, 241)";
+    });
+});
+
+click.forEach(button => {
+    button.addEventListener("drag", () => {
+        button.style.backgroundColor = "rgb(241, 241, 241)";
+    });
+});
+
+click.forEach(button => {
+    button.addEventListener("drop", () => {
+        button.style.backgroundColor = "rgb(241, 241, 241)";
+    });
+});
+
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     const choices = ["ROCK", "PAPER", "SCISSOR"];
     return choices[choice];
 }
-
+/* this function is no longer necessary
 function getHumanChoice() {
     let choice = prompt("Rock, paper, or scissor?");
     return choice;
-}
+} */
 
 function playGame() {
     humanChoice = getHumanChoice().toUpperCase();
